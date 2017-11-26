7bbccc4cc5e83c3b578cd38eb649efaf5fb0e9ca
Loi Truong <tgialoimtr@gmail.com>
2017-11-26 13:19:47 +0700

    db

--- src/main/resources/db/crebas.sql
+++ src/main/resources/db/crebas.sql
@@ -10,6 +10,7 @@ create database conference;
 use conference;
 
 drop table if exists user;
+drop table if exists submission;
 
 /*==============================================================*/
 /* Table: user                                                  */
@@ -17,17 +18,33 @@ drop table if exists user;
 create table user
 
 (
-   user_id              int not null,
+   user_id              int not null AUTO_INCREMENT,
    user_name            char(100),
    first_name           char(100),
    last_name            char(100),
-   midle_name           char(100),
-   academic             char(100),
-   zipcode              int,
+   phone_number         char(40),
+   academic_title       char(100),
+   address              char(100),
    email                char(100),
    password             char(255),
    repassword           char(255),
-   orgnization          char(255),
+   organization         char(255),
    primary key (user_id)
 );
 
+/*==============================================================*/
+/* Table: SUBMSSION                                             */
+/*==============================================================*/
+create table submission
+(
+   submission_id            int not null AUTO_INCREMENT,
+   user_id              integer,
+   track_id           int,
+   title                text,
+   authors              text,
+   pdf_file             longblob,
+   tex_file            longblob,
+   abstract              text,
+   keyword              text,
+   primary key (submission_id)
+);
