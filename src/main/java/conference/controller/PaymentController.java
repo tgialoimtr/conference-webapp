d3e88dd37204d521d9c8866726e95359d4402fbb
triicst <tri.pham.hpc@gmail.com>
2017-11-27 07:10:04 -1000

    payment

---
+++ src/main/java/conference/controller/PaymentController.java
@@ -0,0 +1,18 @@
+package conference.controller;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PaymentController {
+	@RequestMapping(value = "/payment")
+	public String payment() {
+		return "payment";
+	}
+
+}
