149ef8359a273ada28d62b114f0a964f0a50ba28
triicst <tri.pham.hpc@gmail.com>
2017-11-27 06:35:38 -1000

    reviewercontroller

---
+++ src/main/java/conference/controller/ReviewController.java
@@ -0,0 +1,19 @@
+package conference.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ReviewController {
+	@RequestMapping(value = "/review")
+	public String review() {
+		return "reviewer";
+	}
+
+}
